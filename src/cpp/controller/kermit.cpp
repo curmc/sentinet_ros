55ec49558b9655c6976ffadd864f60ade3e6a8d5
Laura Beisser <laura.beisser@colorado.edu>
2020-02-11 17:48:24 -0700

    small change

--- src/cpp/controller/kermit.cpp
+++ src/cpp/controller/kermit.cpp
@@ -12,6 +12,16 @@ void DriveTrain::stop() {
         angular_vel = 0.0f;
 }
 
+void DriveTrain::moveForward(){
+
+}
+
 void DriveTrain::loop() {}
 
 } // namespace rmt
+
+
+//write publisher that publishes these basic values to cmd_vel_topic
+//publish the geometry_messages_twist twist
+//Look at wii controller.cpp under nodes for a reference on how to publish things
+//Go into nodes and make an executable, add to Cmake, call functions
