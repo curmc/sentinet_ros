900896e98abf58f3687dd606b16e2cac3371ed16
PaperXLV <samuel.j.williams@colorado.edu>
2020-02-14 12:42:40 -0700

    basic tests

--- test/utest.cpp
+++ test/utest.cpp
@@ -1,6 +1,23 @@
 #include <gtest/gtest.h>
 #include <ros/ros.h>
 
+#include "kermit/kernel/Kernel.hpp"
+
+/* Gtest
+ * For a primer, visit:
+ * https://github.com/google/googletest/blob/master/googletest/docs/primer.md
+ */
+
+class HelloWorld {};
+
+TEST(HelloWorld, BasicTest) { ASSERT_EQ(1, 1); }
+
+TEST(KernelTest, InvalidTeensy) {
+        Kernel k{};
+        // I feel like this should return false
+        ASSERT_TRUE(k.initialize_teensy("aaaaaa"));
+}
+
 int main(int argc, char *argv[]) {
         testing::InitGoogleTest(&argc, argv);
         ros::init(argc, argv, "tester");
